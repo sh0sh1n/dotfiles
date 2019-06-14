bbf59cd373104aef0432cb81b83a925c2e4f5b65
Shohan Hasan <shohan.hasan.x6@gmail.com>
2019-06-14 15:25:47 -0400

    Fix ctrl+f autocompletion in vi mode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,16 @@ eval (python3 -m virtualfish auto_activation compat_aliases projects)
 
 # enable vi mode
 fish_vi_key_bindings
+# restore ctrl+f for autocomplete
+function fish_user_key_bindings
+    for mode in insert default visual
+	bind -M $mode \cf forward-char
+    end
+end
 
 # vim default editor
 set -U EDITOR vim
 
 # Enable nerd fonts
 set -g nerd_fonts yes
+
